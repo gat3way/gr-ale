8108def1629379b026f4ac7fe8f6b1cd92daaa1c
gat3way <gat3way@debian>
2015-12-30 02:58:09 +0200

    copyright

--- lib/decode_ff_impl.h
+++ lib/decode_ff_impl.h
@@ -1,6 +1,6 @@
 /* -*- c++ -*- */
 /* 
- * Copyright 2015 <+YOU OR YOUR COMPANY+>.
+ * Copyright 2015 Milen Rangelov <gat3way@gmail.com>
  * 
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
